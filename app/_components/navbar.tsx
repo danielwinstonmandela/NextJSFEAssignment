56ed0747835f925723418fe1e6b729252b95ff7e
danielwinstonmandela <danielwinstonmandela@mail.ugm.ac.id>
2024-09-02 22:48:06 +0700

    w

---
+++ app/_components/navbar.tsx
@@ -0,0 +1,21 @@
+"use client"; // Add this line at the top
+
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+
+export default function Navbar() {
+    const pathname = usePathname()
+
+    const links = [
+        {href: "/", label: "Home"},  {href: "/dashboard", label: "Dashboard"},  {href: "/blog", label: "Blog"},  {href: "/contact", label: "Contact"},
+    ]
+
+    return (
+        <nav className="bg-blue-600 text=white flex gap-8 py-4 px-8">
+            {links.map(link => (
+                <Link href={link.href} style={pathname == link.href ? {textDecorationLine: "underline"}: {}}>{link.label}</Link>
+            ))}
+
+        </nav>
+    )
+}
---
+++ app/dashboard/page.tsx
@@ -0,0 +1,8 @@
+export default function dashboard() {
+    return (
+        <div>
+            hello world ! this is the dashboard page
+            <div>this should link to the another page</div>
+        </div>
+    )
+}
